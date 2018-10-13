8e203b48a2c7ef04df92dcf8b3aa9dcd4a8bdcee
GaoangW <gaoang.wang1991@gmail.com>
2018-10-12 21:07:21 -0700

    10/12/2018
    add gaussian regression in post processing

--- track_lib.py
+++ track_lib.py
@@ -31,9 +31,293 @@ from skimage.color import rgb2gray
 from skimage.feature import match_descriptors, ORB, plot_matches
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel
 
+def GP_regression(tr_x,tr_y,test_x):
+    A = np.ones((len(tr_x),2))
+    A[:,0] = tr_x[:,0]
+    p = np.matmul(np.linalg.pinv(A),tr_y)
+    mean_tr_y = np.matmul(A,p)
+    A = np.ones((len(test_x),2))
+    A[:,0] = test_x[:,0]
+    mean_test_y = np.matmul(A,p)
+    kernel = ConstantKernel(100,(1e-5, 1e5))*RBF(1, (1e-5, 1e5))+RBF(1, (1e-5, 1e5))
+    gp = GaussianProcessRegressor(kernel=kernel, alpha=1, n_restarts_optimizer=9)
+    gp.fit(tr_x, tr_y-mean_tr_y)
+    test_y, sigma = gp.predict(test_x, return_std=True)
+    test_y = test_y+mean_test_y
+    #import pdb; pdb.set_trace()
+    return test_y
 
-def remove_det(det_M, det_thresh, y_thresh, h_thresh):
+def show_trajectory(tracklet_mat, obj_id):
+    max_len = 60
+    check_fr = np.where(tracklet_mat['xmin_mat'][obj_id,:]!=-1)[0]
+    
+    test_xmin = tracklet_mat['xmin_mat'][obj_id,:].copy()
+    test_ymin = tracklet_mat['ymin_mat'][obj_id,:].copy()
+    test_xmax = tracklet_mat['xmax_mat'][obj_id,:].copy()
+    test_ymax = tracklet_mat['ymax_mat'][obj_id,:].copy()
+    
+    t1 = max(0,check_fr[0]-max_len)
+    t2 = min(check_fr[-1]+max_len,tracklet_mat['xmin_mat'].shape[1]-1)
+    test_t = np.concatenate((np.array(range(t1,check_fr[0])),np.array(range(check_fr[-1],t2))))
+    test_t = test_t.astype(int)
+    aa = np.zeros((len(check_fr),1))
+    bb = np.zeros((len(check_fr),1))
+    cc = np.zeros((len(test_t),1))
+    aa[:,0] = check_fr
+    cc[:,0] = test_t
+    bb[:,0] = tracklet_mat['xmin_mat'][obj_id,check_fr]
+    dd = GP_regression(aa,bb,cc)
+    #import pdb; pdb.set_trace()
+    test_xmin[test_t] = dd[:,0]
+    bb[:,0] = tracklet_mat['ymin_mat'][obj_id,check_fr]
+    dd = GP_regression(aa,bb,cc)
+    test_ymin[test_t] = dd[:,0]
+    bb[:,0] = tracklet_mat['xmax_mat'][obj_id,check_fr]
+    dd = GP_regression(aa,bb,cc)
+    test_xmax[test_t] = dd[:,0]
+    bb[:,0] = tracklet_mat['ymax_mat'][obj_id,check_fr]
+    dd = GP_regression(aa,bb,cc)
+    test_ymax[test_t] = dd[:,0]
+    
+    t_range = np.where(test_xmin!=-1)[0]
+    #if obj_id==2:
+    #    import pdb; pdb.set_trace()
+    plt.plot(t_range,test_xmin[t_range],'k.',t_range,test_ymin[t_range],'k.',
+             t_range,test_xmax[t_range],'k.',t_range,test_ymax[t_range],'k.',
+             check_fr,tracklet_mat['xmin_mat'][obj_id,check_fr],'b.',check_fr,tracklet_mat['ymin_mat'][obj_id,check_fr],'r.',
+            check_fr,tracklet_mat['xmax_mat'][obj_id,check_fr],'g.',check_fr,tracklet_mat['ymax_mat'][obj_id,check_fr],'y.')
+    plt.show()
+    #import pdb; pdb.set_trace()
+    #plt.close('all')
+    return
+
+def track_extend(xmins, ymins, xmaxs, ymaxs, img_size, bnd_margin, min_len, extend_len, reg_thresh, speed_thresh, static_len, fr_id):
+    # img_size = [x,y]
+    
+    extend_xmins = xmins.copy()
+    extend_ymins = ymins.copy()
+    extend_xmaxs = xmaxs.copy()
+    extend_ymaxs = ymaxs.copy()
+    N_fr = len(xmins)
+    
+    check_flag = 0
+    fr_idx = np.where(xmins!=-1)[0]
+    if len(fr_idx)==0:
+        check_flag = 1
+        return check_flag,extend_xmins,extend_ymins,extend_xmaxs,extend_ymaxs
+    time_interval = [np.min(fr_idx),np.max(fr_idx)]
+    if time_interval[1]-time_interval[0]+1<min_len:
+        check_flag = 1
+        return check_flag,extend_xmins,extend_ymins,extend_xmaxs,extend_ymaxs
+    
+    x_center = np.zeros((min_len,1))
+    y_center = np.zeros((min_len,1))
+    w = np.zeros((min_len,1))
+    h = np.zeros((min_len,1))
+    
+    for drt in range(2):
+        if drt==0:
+            # start direction
+            start_fr = time_interval[0]
+            end_fr = time_interval[0]+min_len
+            ext_fr = max(time_interval[0]-extend_len,0)
+            
+        else:
+            # end direction
+            if time_interval[1]>=N_fr-1:
+                continue
+            start_fr = time_interval[1]+1-min_len
+            end_fr = time_interval[1]+1
+            ext_fr = min(time_interval[1]+extend_len+1,N_fr-1)
+            
+        A = np.ones((min_len,2))
+        A[:,0] = np.array(range(start_fr,end_fr))
+        w[:,0] = xmaxs[start_fr:end_fr]-xmins[start_fr:end_fr]
+        h[:,0] = ymaxs[start_fr:end_fr]-ymins[start_fr:end_fr]
+        mean_w = 0
+        mean_h = 0
+        if drt==0:
+            mean_w = np.mean(w[int(min_len/2):,0])
+            mean_h = np.mean(h[int(min_len/2):,0])
+        else:
+            mean_w = np.mean(w[0:int(min_len/2),0])
+            mean_h = np.mean(h[0:int(min_len/2),0])
+            
+        dist1 = (ymins[int((start_fr+end_fr)/2)]+ymaxs[int((start_fr+end_fr)/2)])/2
+        dist2 = (xmins[int((start_fr+end_fr)/2)]+xmaxs[int((start_fr+end_fr)/2)])/2
+        v_flag = 0 #top
+        h_flag = 0 #left
+        if dist1>img_size[1]/2:
+            dist1 = img_size[1]-dist1
+            v_flag = 1
+        if dist2>img_size[0]/2:
+            dist2 = img_size[0]-dist2
+            h_flag = 1
+        # top bnd
+        if dist1<dist2 and v_flag==0:
+            x_center[:,0] = (xmins[start_fr:end_fr]+xmaxs[start_fr:end_fr])/2
+            y_center[:,0] = (ymaxs[start_fr:end_fr]-mean_h/2)
+        # bot bnd
+        elif dist1<dist2 and v_flag==1:
+            x_center[:,0] = (xmins[start_fr:end_fr]+xmaxs[start_fr:end_fr])/2
+            y_center[:,0] = (ymins[start_fr:end_fr]+mean_h/2)
+        # left bnd
+        elif dist1>=dist2 and h_flag==0:
+            x_center[:,0] = (xmaxs[start_fr:end_fr]-mean_w/2)
+            y_center[:,0] = (ymins[start_fr:end_fr]+ymaxs[start_fr:end_fr])/2
+        # right bnd
+        elif dist1>=dist2 and h_flag==1:
+            x_center[:,0] = (xmins[start_fr:end_fr]+mean_w/2)
+            y_center[:,0] = (ymins[start_fr:end_fr]+ymaxs[start_fr:end_fr])/2
+            
+        #x_center[:,0] = (xmins[start_fr:end_fr]+xmaxs[start_fr:end_fr])/2
+        #y_center[:,0] = (ymins[start_fr:end_fr]+ymaxs[start_fr:end_fr])/2
+        #if fr_id==10:
+        #    import pdb; pdb.set_trace()
+        px = np.matmul(np.linalg.pinv(A),x_center)
+        err_x = np.sum(np.absolute(np.matmul(A,px)-x_center)/mean_w)/min_len
+        if err_x>reg_thresh: # trajectory cannot be predicted
+            continue
+        py = np.matmul(np.linalg.pinv(A),y_center)
+        err_y = np.sum(np.absolute(np.matmul(A,py)-y_center)/mean_h)/min_len
+        if err_y>reg_thresh: # trajectory cannot be predicted
+            continue
+    
+        # slow motion check
+        static_flag = 0
+        diff_x = abs((xmins[time_interval[1]]+xmaxs[time_interval[1]])/2-(xmins[time_interval[0]]+xmaxs[time_interval[0]])/2)
+        diff_y = abs((ymins[time_interval[1]]+ymaxs[time_interval[1]])/2-(ymins[time_interval[0]]+ymaxs[time_interval[0]])/2)
+        speed = np.sqrt(np.power(diff_x,2)+np.power(diff_y,2))/(time_interval[1]-time_interval[0]+1)
+        if speed<speed_thresh and time_interval[1]-time_interval[0]>static_len: # static person
+            static_flag = 1
+    
+        #ext_fr = max(time_interval[0]-extend_len,0)
+        if static_flag==1:
+            mean_x = np.mean(x_center[:,0])
+            mean_y = np.mean(y_center[:,0])
+            #mean_w = np.mean(w[:,0])
+            #mean_h = np.mean(h[:,0])
+            if drt==0:
+                extend_xmins[0:time_interval[0]] = mean_x-mean_w/2
+                extend_ymins[0:time_interval[0]] = mean_y-mean_h/2
+                extend_xmaxs[0:time_interval[0]] = mean_x+mean_w/2
+                extend_ymaxs[0:time_interval[0]] = mean_y+mean_h/2
+            else:
+                if time_interval[1]<N_fr-1:
+                    extend_xmins[time_interval[1]+1:] = mean_x-mean_w/2
+                    extend_ymins[time_interval[1]+1:] = mean_y-mean_h/2
+                    extend_xmaxs[time_interval[1]+1:] = mean_x+mean_w/2
+                    extend_ymaxs[time_interval[1]+1:] = mean_y+mean_h/2
+                
+        else:
+            if drt==0:
+                x0 = x_center[0,0]
+                y0 = y_center[0,0]
+                t1 = ext_fr
+                t2 = time_interval[0]
+            else:
+                x0 = x_center[-1,0]
+                y0 = y_center[-1,0]
+                t1 = time_interval[1]+1
+                t2 = ext_fr
+            # check whether the track near img bnd
+            if abs(x0)>bnd_margin and abs(img_size[0]-x0)>bnd_margin and abs(y0)>bnd_margin and abs(img_size[1]-y0)>bnd_margin:
+                continue
+        
+            t_test = np.array(range(t1,t2))
+            test_t = np.zeros((len(t_test),1))
+            test_t[:,0] = t_test
+            N_t = t2-t1
+            if N_t==0:
+                continue
+            A = np.ones((N_t,2))
+            A[:,0] = t_test
+            
+            tr_t = np.zeros((end_fr-start_fr,1))
+            tr_t[:,0] = np.array(range(start_fr,end_fr))
+            tr_x = np.zeros((end_fr-start_fr,1))
+            tr_x[:,0] = xmins[start_fr:end_fr]
+            tr_y = np.zeros((end_fr-start_fr,1))
+            tr_y[:,0] = ymins[start_fr:end_fr]
+            tr_w = np.zeros((end_fr-start_fr,1))
+            tr_w[:,0] = xmaxs[start_fr:end_fr]-xmins[start_fr:end_fr]
+            tr_h = np.zeros((end_fr-start_fr,1))
+            tr_h[:,0] = ymaxs[start_fr:end_fr]-ymins[start_fr:end_fr]
+
+            test_x = GP_regression(tr_t,tr_x,test_t)
+            test_y = GP_regression(tr_t,tr_y,test_t)
+            test_w = GP_regression(tr_t,tr_w,test_t)
+            test_h = GP_regression(tr_t,tr_h,test_t)
+
+            if drt==0:
+
+                max_idx = np.where(np.logical_or(test_h<test_w,test_w<test_h/4))[0]
+                if len(max_idx)!=0:
+                    relative_t = int(np.max(max_idx))
+                    max_t = int(np.max(max_idx)+t1)
+                else:
+                    relative_t = 0
+                    max_t = int(t1)
+                extend_xmins[max_t:time_interval[0]] = test_x[relative_t:,0]
+                extend_ymins[max_t:time_interval[0]] = test_y[relative_t:,0]
+                extend_xmaxs[max_t:time_interval[0]] = test_x[relative_t:,0]+test_w[relative_t:,0]
+                extend_ymaxs[max_t:time_interval[0]] = test_y[relative_t:,0]+test_h[relative_t:,0]
+            else:
+
+                min_idx = np.where(np.logical_or(test_h<test_w,test_w<test_h/4))[0]
+                if len(min_idx)!=0:
+                    relative_t = int(np.min(min_idx))
+                    min_t = int(np.min(min_idx)+t1)
+                else:
+                    relative_t = 0
+                    min_t = int(t1)
+                extend_xmins[t1:min_t] = test_x[0:relative_t,0]
+                extend_ymins[t1:min_t] = test_y[0:relative_t,0]
+                extend_xmaxs[t1:min_t] = test_x[0:relative_t,0]+test_w[0:relative_t,0]
+                extend_ymaxs[t1:min_t] = test_y[0:relative_t,0]+test_h[0:relative_t,0]
+
+    #import pdb; pdb.set_trace()
+    # constraint output inside image  
+    neg_idx = np.where(extend_xmins==-1)[0]
+    extend_xmins[extend_xmins<1] = 1
+    extend_xmins[extend_xmins>img_size[0]-1] = img_size[0]-1
+    extend_ymins[extend_ymins<1] = 1
+    extend_ymins[extend_ymins>img_size[1]-1] = img_size[1]-1
+    extend_xmaxs[extend_xmaxs<1] = 1
+    extend_xmaxs[extend_xmaxs>img_size[0]-1] = img_size[0]-1
+    extend_ymaxs[extend_ymaxs<1] = 1
+    extend_ymaxs[extend_ymaxs>img_size[1]-1] = img_size[1]-1
+    if len(neg_idx)!=0:
+        extend_xmins[neg_idx] = -1
+        extend_ymins[neg_idx] = -1
+        extend_xmaxs[neg_idx] = -1
+        extend_ymaxs[neg_idx] = -1
+        
+    #if fr_id==10:
+    #    import pdb; pdb.set_trace()
+    return check_flag,extend_xmins,extend_ymins,extend_xmaxs,extend_ymaxs
+    
+def check_bbox_near_img_bnd(bbox, img_size, margin):
+    # img_size = [x,y]
+    xmin = bbox[0,0]
+    ymin = bbox[0,1]
+    xmax = bbox[0,2]+bbox[0,0]
+    ymax = bbox[0,3]+bbox[0,1]
+    
+    check_flag = 0
+    if xmin<margin or ymin<margin:
+        check_flag = 1
+        return check_flag
+    if img_size[0]-xmax<margin or img_size[1]-ymax<margin:
+        check_flag = 1
+        return check_flag
+    
+    return check_flag
+
+def remove_det(det_M, det_thresh, y_thresh, h_thresh, y_thresh2, ratio_1, h_thresh2):
     
     remove_idx = []
     
@@ -47,9 +331,24 @@ def remove_det(det_M, det_thresh, y_thresh, h_thresh):
         if det_M[n,2]<y_thresh:
             remove_idx.append(n)
             
+    # remove det below the ground plane
+    for n in range(len(det_M)):
+        if det_M[n,2]>y_thresh2:
+            remove_idx.append(n)
+    
+    # remove thin objects
+    for n in range(len(det_M)):
+        if (det_M[n,4]/det_M[n,3])>ratio_1:
+            remove_idx.append(n)
+            
     # remove small object
     for n in range(len(det_M)):
         if det_M[n,4]<h_thresh:
+            remove_idx.append(n)
+            
+    # remove large object
+    for n in range(len(det_M)):
+        if det_M[n,4]>h_thresh2:
             remove_idx.append(n)
             
     remove_idx = np.array(list(set(remove_idx)),dtype=int)
@@ -178,25 +477,7 @@ def bbox_interp_with_F(F_set, start_fr, end_fr, start_bbox, end_bbox):
         interp_bbox[n,2] = xmaxs[n+1]-xmins[n+1]+1
         interp_bbox[n,3] = ymaxs[n+1]-ymins[n+1]+1
         
-    return interp_bbox      
-                
-
-def check_bbox_near_img_bnd(bbox, img_size, margin):
-    # img_size = [x,y]
-    xmin = bbox[0,0]
-    ymin = bbox[0,1]
-    xmax = bbox[0,2]+bbox[0,0]
-    ymax = bbox[0,3]+bbox[0,1]
-    
-    check_flag = 0
-    if xmin<margin or ymin<margin:
-        check_flag = 1
-        return check_flag
-    if img_size[0]-xmax<margin or img_size[1]-ymax<margin:
-        check_flag = 1
-        return check_flag
-    
-    return check_flag
+    return interp_bbox                      
 
 def pred_bbox_by_F(bbox, F, show_flag, img1, img2):
     #model, _, _, _, _ = estimateF(img1, img2)
@@ -418,19 +699,22 @@ def get_IOU(bbox1, bbox2):
     #import pdb; pdb.set_trace()
     overlap_area = max(0, (x2-x1+1))*max(0, (y2-y1+1))
     ratio = overlap_area/(area1+area2-overlap_area)
-    return ratio
+    return ratio,overlap_area,area1,area2
 
 def get_overlap(bbox1, bbox2): 
     num1 = bbox1.shape[0] 
     num2 = bbox2.shape[0] 
     overlap_mat = np.zeros((num1, num2)) 
+    overlap_area = np.zeros((num1, num2)) 
+    area1 = np.zeros(num1)
+    area2 = np.zeros(num2)
     for n in range(num1): 
         for m in range(num2):
 
             #import pdb; pdb.set_trace()
-            overlap_mat[n,m] = get_IOU(bbox1[n,:], bbox2[m,:])
+            overlap_mat[n,m],overlap_area[n,m],area1[n],area2[m] = get_IOU(bbox1[n,:], bbox2[m,:])
 
-    return overlap_mat
+    return overlap_mat,overlap_area,area1,area2
 
 def load_detection(file_name, dataset):
 
@@ -547,16 +831,16 @@ def load_detection(file_name, dataset):
         cnt = 0
         for n in range(len(f)):
             if mask[n,0]==1:
-                M[cnt,0] = int(float(f[n][0]))+1
+                M[cnt,0] = int(float(f[n][0]))
                 M[cnt,1] = int(float(f[n][1]))
                 M[cnt,2] = int(float(f[n][2]))
                 M[cnt,3] = int(float(f[n][3]))
                 M[cnt,4] = int(float(f[n][4]))
                 M[cnt,5] = float(f[n][10])/100.0
-                M[cnt,6] = float(f[n][5])/723.0
-                M[cnt,7] = float(f[n][7])/723.0
-                M[cnt,8] = float(f[n][8])/723.0
-                M[cnt,9] = float(f[n][9])/723.0
+                M[cnt,6] = float(f[n][5])
+                M[cnt,7] = float(f[n][7])
+                M[cnt,8] = float(f[n][8])
+                M[cnt,9] = float(f[n][9])
                 cnt = cnt+1
             #import pdb; pdb.set_trace()
         return M
@@ -579,7 +863,7 @@ def bbox_associate(overlap_mat, IOU_thresh):
     idx2 = np.array(idx2)
     return idx1, idx2
 
-def merge_bbox(bbox, IOU_thresh, det_score): 
+def merge_bbox(bbox, IOU_thresh, det_score, merge_mode): 
     num = bbox.shape[0] 
     cand_idx = np.ones((num,1)) 
     for n1 in range(num-1): 
@@ -592,14 +876,25 @@ def merge_bbox(bbox, IOU_thresh, det_score):
             b = np.zeros((1,4))
             a[0,:] = bbox[n1,:]
             b[0,:] = bbox[n2,:]
-            r = get_overlap(a, b)[0,0]
+            r,overlap_area,area1,area2 = get_overlap(a, b)
+            r = r[0,0]
+            overlap_area = overlap_area[0,0]
+            r1 = overlap_area/area1[0]
+            r2 = overlap_area/area2[0]
             s1 = det_score[n1]
             s2 = det_score[n2]
-            if r>IOU_thresh:
-                if s1>s2:
-                    cand_idx[n2] = 0
-                else:
-                    cand_idx[n1] = 0
+            if merge_mode==0:
+                if r>IOU_thresh:
+                    if s1>s2:
+                        cand_idx[n2] = 0
+                    else:
+                        cand_idx[n1] = 0
+            if merge_mode==1:
+                if r1>IOU_thresh or r2>IOU_thresh:
+                    if s1>s2:
+                        cand_idx[n2] = 0
+                    else:
+                        cand_idx[n1] = 0
     idx = np.where(cand_idx==1)[0]
     new_bbox = bbox[idx,:]
     return idx, new_bbox
