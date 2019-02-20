499d820bb2b5a523884c3427de79f9f7e4e72b58
GaoangW <gaoang.wang1991@gmail.com>
2019-02-20 12:57:33 -0800

    Add files via upload

--- General/MOT_to_UA_Detrac.m
+++ General/MOT_to_UA_Detrac.m
@@ -1,14 +1,19 @@
-function MOT_to_UA_Detrac(gt_file, seq_name, save_folder)
+function MOT_to_UA_Detrac(gt_file, seq_name, save_folder, img_size)
 % gt_file: MOT gt txt
 % seq_name: save name
 % save_folder: save dir
 fileID = fopen(gt_file,'r');
-A = textscan(fileID,'%d %d %d %d %d %d %d %d %f','Delimiter',',');
+A = textscan(fileID,'%d %d %d %d %d %d %d %d %d %d','Delimiter',',');
 fclose(fileID);
 
 M = [A{1},A{2},A{3},A{4},A{5},A{6},A{7},A{8},A{9}];
 max_fr = max(M(:,1));
-max_id = max(M(:,2));
+uniq_ids = unique(M(:,2));
+for n = 1:length(uniq_ids)
+    A{2}(M(:,2)==uniq_ids(n)) = n;
+end
+M(:,2) = A{2};
+max_id = length(uniq_ids);
 X = zeros(max_fr,max_id);
 Y = zeros(max_fr,max_id);
 W = zeros(max_fr,max_id);
@@ -21,5 +26,28 @@ gtInfo.X = X;
 gtInfo.Y = Y;
 gtInfo.W = W;
 gtInfo.H = H;
+
+% visibility
+V = zeros(size(W));
+for n = 1:size(W,1)
+    idx = find(W(n,:)~=0);
+    if length(idx)<=1
+        V(n,idx) = 1;
+        continue
+    end
+    bbox = zeros(length(idx),4);
+    bbox(:,1) = X(n,idx)';
+    bbox(:,2) = Y(n,idx)';
+    bbox(:,3) = W(n,idx)';
+    bbox(:,4) = H(n,idx)';
+    overlapRatio = bboxOverlapRatio(bbox,bbox);
+    for k = 1:length(idx)
+        overlapRatio(k,k) = 0;
+    end
+    max_overlap = max(overlapRatio,[],1);
+    V(n,idx) = 1-max_overlap;
+end
+gtInfo.V = V;
+gtInfo.img_size = img_size;
 save_path = [save_folder,'\',seq_name,'.mat'];
 save(save_path,'gtInfo');
