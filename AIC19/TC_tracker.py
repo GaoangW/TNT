fcdbb52570c16b6577d0d0d51f45170ecb145f51
GaoangW <gaoang.wang1991@gmail.com>
2019-05-31 20:32:24 -0700

    Update TC_tracker.py

--- AIC19/TC_tracker.py
+++ AIC19/TC_tracker.py
@@ -35,26 +35,4 @@ import track_lib
 
 
 track_struct = tracklet_utils_3c.TC_tracker()
-'''
-#cam_list = ['c020','c021','c023','c020','c025','c027','c029','c035']
-cam_list = ['c029']
-for n in range(0,len(cam_list)):
-    seq_name = cam_list[n]
-    img_name = cam_list[n]
-    sub_seq_name = ''
-    file_len = 6
-    ROI_path = 'C:/Users/tangz/OneDrive/Documents/Gaoang/AI_City_2019/s5_MOG2_result/s5_MOG2_result/test_S05_'+cam_list[n]+'/frame/'
-    det_path = 'C:/Users/tangz/OneDrive/Documents/Gaoang/AI_City_2019/aic19-track1-mtmc/test/s05/'+cam_list[n]+'/det/det_ssd512.txt'
-    img_folder = 'C:/Users/tangz/OneDrive/Documents/Gaoang/AI_City_2019/test_img/'+seq_name
-    crop_det_folder = 'C:/Users/tangz/OneDrive/Documents/Gaoang/AI_City_2019/crop_det/'+seq_name+sub_seq_name
-    tracking_img_folder = 'C:/Users/tangz/OneDrive/Documents/Gaoang/AI_City_2019/tracking_img/'+seq_name+sub_seq_name
-    tracking_video_path = 'C:/Users/tangz/OneDrive/Documents/Gaoang/AI_City_2019/tracking_video/'+seq_name+sub_seq_name+'.avi'
-    appear_mat_path = 'C:/Users/tangz/OneDrive/Documents/Gaoang/AI_City_2019/appear_mat/'+seq_name+'.obj'
-    txt_result_path = 'C:/Users/tangz/OneDrive/Documents/Gaoang/AI_City_2019/txt_result/'+seq_name+sub_seq_name+'.txt'
-    track_struct_path = 'C:/Users/tangz/OneDrive/Documents/Gaoang/AI_City_2019/track_struct/'+seq_name+sub_seq_name+'.obj'
-    
-    track_struct, img_size = tracklet_utils_3c_fun.TC_tracker(seq_name, img_name, sub_seq_name, file_len, ROI_path, det_path, img_folder, crop_det_folder, tracking_img_folder, tracking_video_path, appear_mat_path, txt_result_path, track_struct_path)
-    
-    #post_deep_match_fun.post_deep_match_fun(seq_name,img_name,img_size)
-    #import pdb; pdb.set_trace()
-'''
+
